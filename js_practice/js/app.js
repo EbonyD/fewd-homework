7624cbff3e67b4df949c22af802d9f37c5559714
Ebony <Ebony@Ebonys-MacBook-Pro.local>
2016-03-31 19:06:31 -0700

    update

---
+++ js_practice/js/app.js
@@ -0,0 +1,41 @@
+$(document).ready(function(){
+
+
+  alert("Wait, don't leave this page yet. Click here for a special offer!")
+  $("h1").click(
+    function(){
+      alert("You clicked the headline")
+    }
+  )
+
+
+// var color = prompt("What color?")
+// console.log(color)
+
+
+// color = "black"
+// console.log(color)
+
+var firstNum = 1
+var secondNum = 1
+var sum = firstNum * secondNum
+console.log("Your total is: ")
+console.log(answer)
+
+if( answer == 1 ){
+  alert("WooHoo!! The answer is one!!!!")
+
+
+  var lighswitch = true
+
+  if( lightswitch == true){
+    alert("The lights are on"
+    )
+  }
+
+}
+
+
+
+
+});
